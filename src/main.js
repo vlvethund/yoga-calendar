2db1b161b416541f1561ab97135d6046784c268c
hund <utiful2004@gmail.com>
2021-12-22 17:52:17 +0900

    init firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
+import { initializeApp } from "firebase/app";
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYhP9N8D0OHJnsmA0hPXQyUqRW1W5D7ek",
+  authDomain: "mindeulle.firebaseapp.com",
+  projectId: "mindeulle",
+  storageBucket: "mindeulle.appspot.com",
+  messagingSenderId: "602199815410",
+  appId: "1:602199815410:web:7abd50a6c31dea405c0c6b",
+  measurementId: "G-PRB2J1N5C8"
+};
+
+initializeApp(firebaseConfig);
 
 new Vue({
   vuetify,
